7c50d7d4fb1b0cdad3fb058fb65ff06cc004209b
Afif Patel <afifpatel@hotmail.co.uk>
2022-08-06 17:14:02 +0100

    timeblocks with time, description and buttons created

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+// Display today's day and date
+
+// loop over time blocks
+$(".description").each(function () {
+  var blockTime = parseInt($(this).parent().attr("id").replace("hour", ""));
+  var now = moment();
+  var currentTime = now.hour();
+
+  // To check the time and add the classes for background indicators
+  if (blockTime < currentTime) {
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+    $(this).addClass("past");
+  } else if (blockTime === currentTime) {
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+    $(this).addClass("present");
+  } else {
+    $(this).removeClass("present");
+    $(this).removeClass("past");
+    $(this).addClass("future");
+  }
+});
+
+for (let index = 9; index < 17; index++) {
+  $(`#hour${index} .description`).val(localStorage.getItem(`hour${index}`));
+}
+
+$(".saveBtn").on("click", function () {
+  var text = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+
+  localStorage.setItem(time, text);
+});
