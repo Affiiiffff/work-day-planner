c0ef52ec32fd7e8256c79e9578fc892176e924e4
Afif Patel <afifpatel@hotmail.co.uk>
2022-08-06 17:17:11 +0100

    final

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,8 @@
-// Display today's day and date
-
-// loop over time blocks
 $(".description").each(function () {
   var blockTime = parseInt($(this).parent().attr("id").replace("hour", ""));
   var now = moment();
   var currentTime = now.hour();
 
-  // To check the time and add the classes for background indicators
   if (blockTime < currentTime) {
     $(this).removeClass("future");
     $(this).removeClass("present");
@@ -25,3 +21,10 @@ $(".description").each(function () {
 for (let index = 9; index < 17; index++) {
   $(`#hour${index} .description`).val(localStorage.getItem(`hour${index}`));
 }
+
+$(".saveBtn").on("click", function () {
+  var text = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+
+  localStorage.setItem(time, text);
+});
